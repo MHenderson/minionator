5df0762e70a493a3458ab34b10b54ce8ebb4f1ef
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-11-20 11:57:03 -0500

    A more specific implementation of watchvecneq, for now.

--- R/watchvecneq.R
+++ R/watchvecneq.R
@@ -1,5 +1,5 @@
 #' Disequality constraint
 #' @export
-watchvecneq <- function(i, j, k, l, m, n, o, p, name = "l") {
-  sprintf("watchvecneq([%s[%s,%s],%s[%s,%s]],[%s[%s,%s],%s[%s,%s]])", name, i, j, name, k, l, name, m, n, name, o, p)
+watchvecneq <- function(i, j, k, l) {
+  sprintf("watchvecneq([l[%s,%s],l[%s,%s]],[l[%s,%s],l[%s,%s]])", i, j, j, i, k, l, l, k)
 }
