d295e0f7b62328b4920d07bb43f6dc623dff6cdf
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-11-21 16:38:34 -0500

    Fix tests for watchvecneq function.

--- tests/testthat/test-watchvecneq.R
+++ tests/testthat/test-watchvecneq.R
@@ -4,5 +4,5 @@ context("watchvecneq")
 ## TODO: Add more tests
 
 test_that("watchvecneq works", {
-  expect_equal(watchvecneq(0, 4, 4, 0, 0, 2, 2, 0), "watchvecneq([l[0,4],l[4,0]],[l[0,2],l[2,0]])")
+  expect_equal(watchvecneq(0, 4, 4, 0), "watchvecneq([l[0,4],l[4,0]],[l[4,0],l[0,4]])")
 })
