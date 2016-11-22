3c58f8ab47dd10a0513267c4995cac7dcbc54798
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-11-22 15:27:03 -0500

    Make the hsols function parse its hole_spec argument.

--- R/hsols.R
+++ R/hsols.R
@@ -1,7 +1,9 @@
 #' HSOLS generator
 #' @export
-hsols <- function() {
+hsols <- function(hole_spec) {
+  s <- deparse(substitute(hole_spec))
+  n <- sum(unlist(lapply(parseholes(s), prod)))
   template <- system.file("hsols.minion.rsp", package = "minionator")
-  R.rsp::rsource(template)
-  R.rsp::rfile(template)
+  R.rsp::rsource(template, args = list(N = n))
+  R.rsp::rfile(template, args = list(N = n))
 }
