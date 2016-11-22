8e97370552d52f9f177ab4945297325dcadc98b1
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-11-22 15:26:31 -0500

    Make the parseholes function work with strings.

--- R/parseholes.R
+++ R/parseholes.R
@@ -1,11 +1,10 @@
 #' Parse holes description strings
 #'
-#' @param x Hole description
+#' @param s Hole description string
 #' @examples
 #' parseholes(2^2:3^2)
 #' @export
-parseholes <- function(x) {
-  s <- deparse(substitute(x))
+parseholes <- function(s) {
   ss <- strsplit(s, ":")[[1]]
   lapply(strsplit(ss, "\\^"), as.numeric)
 }
