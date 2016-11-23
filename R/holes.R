b6bc39a5cd90812b6173056845f0f398bffce08b
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-11-23 14:15:08 -0500

    Generalise holes function to all hole specs.

--- R/holes.R
+++ R/holes.R
@@ -1,4 +1,19 @@
-# Gives the indices which are empty from row/column x for a 2^4 HSOLS
-holes <- function(x) {
-  (2*floor(x/2)):(2*floor(x/2) + 1)
+#' Which rows/columns are empty?
+#'
+#' Gives the indices which are empty from row/column x
+#'
+#' @param hole_spec
+#'
+#' @return
+#' @export
+#'
+#' @examples
+holes <- function(hole_spec) {
+  k <- unlist(lapply(hole_spec, function(x) rep(x[1], x[2])))
+  sk <- c(0, cumsum(k)[-length(k)])
+  begin <- rep(sk, k)
+  end <- rep(k + sk - 1, k)
+  function(i) {
+    return(begin[i + 1]:end[i + 1])
+  }
 }
