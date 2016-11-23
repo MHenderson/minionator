6124d20a379aa6bef1d7e3418c5065beb862c0fd
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-11-23 14:35:52 -0500

    New function: empties.

---
+++ R/empties.R
@@ -0,0 +1,8 @@
+#' @export
+empties <- function(hole_spec) {
+  h <- holes(hole_spec)
+  df <- t(data.frame(lapply(0:7, function(x) (rbind(rep(x, length(h(x))), h(x))))))
+  rownames(df) <- 0:15
+  colnames(df) <- c("i", "j")
+  return(df)
+}
