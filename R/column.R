e856ea07e24aaaebe5c9d5f4fb2e77a9b84fe9bd
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-11-24 14:07:54 -0500

    Generalise/fix implementation of row/column.

--- R/column.R
+++ R/column.R
@@ -10,6 +10,6 @@ column <- function(hole_spec) {
   h <- holes(hole_spec)
   n <- size(hole_spec)
   function(x) {
-    alldiff(l(setdiff(0:(n - 1), h(x)), rep(x, n - 2)))
+    alldiff(l(setdiff(0:(n - 1), h(x)), rep(x, n - length(h(x)))))
   }
 }
--- R/row.R
+++ R/row.R
@@ -10,6 +10,6 @@ row <- function(hole_spec) {
   h <- holes(hole_spec)
   n <- size(hole_spec)
   function(x) {
-    alldiff(l(rep(x, n - 2), setdiff(0:(n - 1), h(x))))
+    alldiff(l(rep(x, n - length(h(x))), setdiff(0:(n - 1), h(x))))
   }
 }
