722f6197d400231633119a54bf949546a16b3576
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-11-24 14:08:32 -0500

    Generalise the empties function.

--- R/empties.R
+++ R/empties.R
@@ -5,8 +5,10 @@
 #' @export
 empties <- function(hole_spec) {
   h <- holes(hole_spec)
-  df <- t(data.frame(lapply(0:7, function(x) (rbind(rep(x, length(h(x))), h(x))))))
-  rownames(df) <- 0:15
+  n <- size(hole_spec)
+  n_empty <- size(lapply(hole_spec, function(x) { return(c(x[1]^2, x[-1])) }))
+  df <- t(data.frame(lapply(0:(n - 1), function(x) (rbind(rep(x, length(h(x))), h(x))))))
+  rownames(df) <- 0:(n_empty - 1)
   colnames(df) <- c("i", "j")
-  return(df)
+  return(data.frame(df))
 }
