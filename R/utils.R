f7bc209e7a02bbde0df75ff2d4f068420ed08e9d
Matthew Henderson <matthew.james.henderson@gmail.com>
2020-07-14 14:21:26 +0100

    fix warnings and notes

--- R/utils.R
+++ R/utils.R
@@ -1,19 +1,21 @@
+#' @importFrom rlang .data
 make_args <- function(x) {
   x %>%
     dplyr::mutate(id = glue::glue("{name}[{row},{col}]") %>% as.character()) %>%
-    dplyr::summarise(args = glue::glue_collapse(id, sep = ",")) %>%
+    dplyr::summarise(args = glue::glue_collapse(.data$id, sep = ",")) %>%
     dplyr::pull(args)
 }
 
+#' @importFrom rlang .data
 discrete_to_str <- function(X) {
   X %>%
-  dplyr::group_by(name) %>%
+  dplyr::group_by(.data$name) %>%
   dplyr::summarise(
-    nrow  = max(row) + 1,
-    ncol  = max(col) + 1,
-    lower = dplyr::first(lower),
-    upper = dplyr::first(upper),
-    type  = dplyr::first(type)
+    nrow  = max(.data$row) + 1,
+    ncol  = max(.data$col) + 1,
+    lower = dplyr::first(.data$lower),
+    upper = dplyr::first(.data$upper),
+    type  = dplyr::first(.data$type)
   ) %>%
     glue::glue_data("
   {type} {name}[{nrow},{ncol}] {{{lower}..{upper}}}
